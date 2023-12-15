3d7e5052dc60e19c1e043e0a42677f58304d7746
Deependra Kumawat <kumawatdeependra43@gmail.com>
2023-12-16 00:33:29 +0530

    Sign up as a Rider completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Headers from './components/headers';
 import Main from './components/main';
 import login from './components/login';
 import Signup from './components/signup';
+import Signupforrider from './components/signupForRider';
 function App() {
   return (
     <div className="App">
       <Headers/>
       {/* <Main/> */}
-      <Signup/>
+      
+      <Signupforrider/>
     </div>
   );
 }
