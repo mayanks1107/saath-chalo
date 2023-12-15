6139f29f311e56eecb529e30bc325742c16013ce
Deependra Kumawat <kumawatdeependra43@gmail.com>
2023-12-16 00:12:27 +0530

    sign up page frontend done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Headers from './components/headers';
 import Main from './components/main';
+import login from './components/login';
+import Signup from './components/signup';
 function App() {
   return (
     <div className="App">
       <Headers/>
-      <Main/>
+      {/* <Main/> */}
+      <Signup/>
     </div>
   );
 }
