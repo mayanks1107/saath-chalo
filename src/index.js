2fbbb063a6cd7fbb41a5a9f36d322431ab764eb6
Shashwat Chouhan <ishashwat@tutanota.com>
2023-12-15 23:34:46 +0530

    login page with material ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import "bootstrap/dist/js/bootstrap";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
