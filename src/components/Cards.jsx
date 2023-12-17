b460478aebffa1ed2dbf8bf0cee926ecf7eec36b
Mayank Srivastava <mayanks03@outlook.com>
2023-12-17 15:33:43 +0530

    home page

---
+++ src/components/Cards.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Cards.css"
+export default function Cards(){
+    return(
+        <div className="cards">
+            <div class="ban-1 card" style={{"width": "20rem"}}>
+              <div class="card-body">
+                <h5 class="card-title">Your choice of transportation at affordable rates.</h5>
+
+                <p class="card-text">Regardless of your destination, whether by bus or carpool, discover the ideal transportation option from our extensive selection of routes, all offered at budget-friendly rates.</p>
+
+              </div>
+            </div>
+            <div class="ban-1 c-2 card" style={{"width": "23rem"}}>
+              <div class="card-body">
+                <h5 class="card-title">Rely on those you journey with</h5>
+                
+                <p class="card-text">We invest the time to familiarize ourselves with every member and bus partner. We meticulously review reviews, profiles, and IDs, ensuring you have a clear understanding of your travel companions. Book your ride with confidence on our secure platform.</p>
+                
+              </div>
+            </div>
+            <div class="ban-1 card" style={{"width": "20rem"}}>
+              <div class="card-body">
+                <h5 class="card-title">Scroll, click, tap, and you're on your way!</h5>
+                
+                <p class="card-text">Securing a ride is now effortlessly convenient! With our user-friendly app driven by cutting-edge technology, booking a nearby ride takes just minutes.</p>
+                
+              </div>
+            </div>
+        </div>
+    )
+}
