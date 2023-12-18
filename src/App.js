15ba9db4e205cddf7a87d19286e803f015b53f1f
Deependra Kumawat <kumawatdeependra43@gmail.com>
2023-12-18 19:46:43 +0530

    security

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './components/home';
 import Login from './components/login';
+import Security from './components/Security'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 function App() {
   return(
@@ -9,6 +10,8 @@ function App() {
             <Routes>
                 <Route exact path="/" element={<Home/>}/>
                 <Route exact path="/login" element={<Login/>}/>
+                <Route exact path="/Security" element={<Security/>}/>
+
                 
             </Routes>
         </Router>
