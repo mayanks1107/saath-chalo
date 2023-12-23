e16ad9769bf8d2949404943ba0be7df669a5a9d2
raiayush2610 <ayushayushravi@gmail.com>
2023-12-23 13:08:09 +0530

    css is change

--- src/components/createRide.jsx
+++ src/components/createRide.jsx
@@ -7,35 +7,35 @@ function createRide() {
         <div className='create-ride'>
             <Headers/>
             <div className='form-create'>
-                <label for="vehicleNumber">Vehicle Number</label>
+                <label for="vehicleNumber"><b>Vehicle Number</b></label>
                 <input className='v-no input-create' type="text" name="vehicleNumber" id="vehicleNumber" placeholder='Vehicle Number'/>
                 <br/>
                 <br/>
-                <label for="source">Source</label>
+                <label for="source"><b>Source</b></label>
                 <input className='so input-create' type="text" name="source" id="source" placeholder='Source' />
                 <br/>
                 <br/>
-                <label for="destination">Destination</label>
+                <label for="destination"><b>Destination</b></label>
                 <input className='dest input-create' type="text" name="destination" id="destination" placeholder='Destination' />
                 <br/>
                 <br/>
-                <label for="typeOfTrip">Type Of Trip</label>
+                <label for="typeOfTrip"><b>Type Of Trip</b></label>
                 <input className='trip input-create' type="check" name="typeOfTrip" id="typeOfTrip" placeholder='1 way / 2 way'/>
                 <br/>
                 <br/>
-                <label for="dateOfTrip">Date Of Trip</label>
+                <label for="dateOfTrip"><b>Date Of Trip</b></label>
                 <input className='date input-create' type="date" name="dateOfTrip" id="dateOfTrip" placeholder='Date'/>
                 <br/>
                 <br/>
-                <label for="timeOfTrip">Time Of Trip</label>
+                <label for="timeOfTrip"><b>Time Of Trip</b></label>
                 <input className='time input-create' type="time" name="timeOfTrip" id="timeOfTrip" placeholder='Time'/>
                 <br/>
                 <br/>
-                <label for="availableSeat">Available Seat</label>
+                <label for="availableSeat"><b>Available Seat</b></label>
                 <input className='av-seat input-create' type="number" name="availableSeat" id="availableSeat" placeholder='Available Seat'/>
                 <br/>
                 <br/>
-                <label for="phoneNumber">Phone Number</label>
+                <label for="phoneNumber"><b>Phone Number</b></label>
                 <input className='phone input-create' type="number" name="phoneNumber" id="phoneNumber" placeholder='Phone Number'/>
                 <img src={'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp'} alt='img-create' className='img-create'/>
             </div>
