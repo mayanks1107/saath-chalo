ae65d8eadeef7beaafc1c9bf749e50d38024278a
Deependra Kumawat <kumawatdeependra43@gmail.com>
2024-01-03 16:56:42 +0530

    create ride button

--- src/components/createRide.jsx
+++ src/components/createRide.jsx
@@ -5,41 +5,47 @@ import Footer from './Footer/Footer';
 function createRide() {
     return (
         <div className='create-ride'>
-            <Headers/>
+            <Headers />
             <div className='form-create'>
                 <label for="vehicleNumber"><b>Vehicle Number</b></label>
-                <input className='v-no input-create' type="text" name="vehicleNumber" id="vehicleNumber" placeholder='Vehicle Number'/>
-                <br/>
-                <br/>
+                <input className='v-no input-create' type="text" name="vehicleNumber" id="vehicleNumber" placeholder='Vehicle Number' />
+                <br />
+                <br />
                 <label for="source"><b>Source</b></label>
                 <input className='so input-create' type="text" name="source" id="source" placeholder='Source' />
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <label for="destination"><b>Destination</b></label>
                 <input className='dest input-create' type="text" name="destination" id="destination" placeholder='Destination' />
-                <br/>
-                <br/>
+                <br />
+                <br />
                 <label for="typeOfTrip"><b>Type Of Trip</b></label>
-                <input className='trip input-create' type="check" name="typeOfTrip" id="typeOfTrip" placeholder='1 way / 2 way'/>
-                <br/>
-                <br/>
+                <input className='trip input-create' type="check" name="typeOfTrip" id="typeOfTrip" placeholder='1 way / 2 way' />
+                <br />
+                <br />
                 <label for="dateOfTrip"><b>Date Of Trip</b></label>
-                <input className='date input-create' type="date" name="dateOfTrip" id="dateOfTrip" placeholder='Date'/>
-                <br/>
-                <br/>
+                <input className='date input-create' type="date" name="dateOfTrip" id="dateOfTrip" placeholder='Date' />
+                <br />
+                <br />
                 <label for="timeOfTrip"><b>Time Of Trip</b></label>
-                <input className='time input-create' type="time" name="timeOfTrip" id="timeOfTrip" placeholder='Time'/>
-                <br/>
-                <br/>
+                <input className='time input-create' type="time" name="timeOfTrip" id="timeOfTrip" placeholder='Time' />
+                <br />
+                <br />
                 <label for="availableSeat"><b>Available Seat</b></label>
-                <input className='av-seat input-create' type="number" name="availableSeat" id="availableSeat" placeholder='Available Seat'/>
-                <br/>
-                <br/>
+                <input className='av-seat input-create' type="number" name="availableSeat" id="availableSeat" placeholder='Available Seat' />
+                <br />
+                <br />
                 <label for="phoneNumber"><b>Phone Number</b></label>
-                <input className='phone input-create' type="number" name="phoneNumber" id="phoneNumber" placeholder='Phone Number'/>
-                <img src={'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp'} alt='img-create' className='img-create'/>
+                <input className='phone input-create' type="number" name="phoneNumber" id="phoneNumber" placeholder='Phone Number' />
+                <img src={'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp'} alt='img-create' className='img-create' />
+
+
             </div>
-            <Footer className="foot"/>
+
+            <div>
+                <input className="createRideButton" type="submit" value="Create Ride" />
+            </div>
+            <Footer className="foot" />
         </div>
     )
 }
