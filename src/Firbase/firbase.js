26e92caf6764ab38daa9697e799b37a0f87bfd6c
ayush2020 <ayush2020@vitbhopal.ac.in>
2024-04-15 11:10:12 +0530

    site-reload-on-particular-page

--- src/Firbase/firbase.js
+++ src/Firbase/firbase.js
@@ -1,24 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAiZC94MvHyJf5S4Eo1EVFxDQxY2-TKl1E",
-  authDomain: "saathchallo.firebaseapp.com",
-  projectId: "saathchallo",
-  storageBucket: "saathchallo.appspot.com",
-  messagingSenderId: "495808524845",
-  appId: "1:495808524845:web:759ea4c04b1142ae4c7ea4",
-  measurementId: "G-PH92KTSX95"
-};
+// saathchallo
 // const firebaseConfig = {
-//   apiKey: "AIzaSyBRhprJlgtp8sI52hjzsfNU_Hy1hhNPa4U",
-//   authDomain: "captone-4af5a.firebaseapp.com",
-//   projectId: "captone-4af5a",
-//   storageBucket: "captone-4af5a.appspot.com",
-//   messagingSenderId: "320128457804",
-//   appId: "1:320128457804:web:1b9102783fb2af83c568e9",
-//   measurementId: "G-QPV5BZ8ZBS"
+//   apiKey: "AIzaSyAiZC94MvHyJf5S4Eo1EVFxDQxY2-TKl1E",
+//   authDomain: "saathchallo.firebaseapp.com",
+//   projectId: "saathchallo",
+//   storageBucket: "saathchallo.appspot.com",
+//   messagingSenderId: "495808524845",
+//   appId: "1:495808524845:web:759ea4c04b1142ae4c7ea4",
+//   measurementId: "G-PH92KTSX95"
 // };
+// phone-otp-357ed
+const firebaseConfig = {
+  apiKey: "AIzaSyAiXlrUmt89Fcn1ik7RQ9mdzBsEvKQbA1E",
+  authDomain: "phone-otp-357ed.firebaseapp.com",
+  projectId: "phone-otp-357ed",
+  storageBucket: "phone-otp-357ed.appspot.com",
+  messagingSenderId: "1011935798317",
+  appId: "1:1011935798317:web:624503212ec0dc7ca6c4a4"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
