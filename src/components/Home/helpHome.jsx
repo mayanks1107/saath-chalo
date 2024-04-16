554a36445801e3f389c680bc51ac26d01182b110
Mayank Srivastava <mayank31rbt@gmail.com>
2024-04-16 17:02:21 +0530

    responsive helpHome

--- src/components/Home/helpHome.jsx
+++ src/components/Home/helpHome.jsx
@@ -17,16 +17,26 @@ export default function HelpHome(){
       const hiddenEle = document.querySelectorAll('.hidden');
       hiddenEle.forEach((el) => observer.observe(el));
     return (
+      <div className="helps">
         <div className="helpHome">
+        
             <h1 className="hidden hh-h1">Carpool Help Centre</h1>
             <h2 className="hidden hh-h2">Why Should We Carpool?</h2>
             <p className="hidden hh-p">Encouraging widespread carpooling would significantly reduce the number of vehicles on the road, resulting in decreased carbon emissions, less pollution, and a more environmentally friendly atmosphere.</p>
+            
             <h3 className="hidden hh-h3 h3-safe">Safe Environment</h3>
-            <img className="hidden safe hh-img" src = {safe} alt="safe Env"/>
+            
             <h3 className="hidden hh-h3 h3-traffic">Reduce Traffic</h3>
-            <img className="hidden traffic hh-img" src = {traffic} alt="traffic"/>
+            
             <h3 className="hidden hh-h3 h3-friends">Make friends</h3>
+           
+            
+        </div>
+        <div className="imgs">
+        <img className="hidden safe hh-img" src = {safe} alt="safe Env"/>
+            <img className="hidden traffic hh-img" src = {traffic} alt="traffic"/>
             <img className="hidden friends hh-img friend-img" src = {friends} alt="friends"/>
+        </div>
         </div>
     )
 }
