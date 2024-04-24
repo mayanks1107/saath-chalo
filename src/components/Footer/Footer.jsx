bc43bde5a389bec6df526f96bc5b1870bb166f82
ayush2020 <ayush2020@vitbhopal.ac.in>
2024-04-24 11:33:39 +0530

    Server-Check

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,15 +5,19 @@ import { FaInstagram } from "react-icons/fa6";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import "./footer.css";
+import { Server } from "../Server/Server";
 export default function Footer(){
   const current = new Date();
   const date = `${current.getFullYear()}`;
+const ServerCheck=()=>{
+  console.warn(Server)
+}
     return(
         <div class="container-foot">
             
               <div class="">
                 
-                <span class="foot-text">&copy; {date} Saath Challo</span>
+                <span class="foot-text" onClick={ServerCheck}>&copy; {date} Saath Challo</span>
               </div>
 
               <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
